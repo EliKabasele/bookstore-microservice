a0f1d117dd7b0cea82030315cc560a906d165893
e.kabaselekabasele <eli.kabaselekabasele@s-managemenet-services.de>
2025-11-22 22:54:24 +0100

    add local project to git

---
+++ catalog-service/src/test/java/com/elikabasele/catalog_service/TestCatalogServiceApplication.java
@@ -0,0 +1,14 @@
+package com.elikabasele.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
+}
